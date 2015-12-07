8a9fe2b59d03d0b4241bca9be452c7e99f183a4c
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-12-07 20:29:56 +0300

    Docs

--- library/Hasql/TH.hs
+++ library/Hasql/TH.hs
@@ -19,6 +19,11 @@ import qualified Data.Text.IO
 -- >migration1 =
 -- >  $(Hasql.TH.readFileAsTransaction "sql/migration-1.sql")
 -- 
+-- Note that so far this function uses a very simple parsing algorithm
+-- to extract individual statements from the file.
+-- The algorithm simply splits the contents by the semicolon.
+-- So make sure that you only apply it to SQL,
+-- which only uses the semicolon for statement-separation.
 readFileAsTransaction :: String -> Q Exp
 readFileAsTransaction path =
   do
