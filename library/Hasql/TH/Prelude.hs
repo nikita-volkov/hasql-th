b0701deb069d07221b24b4bdcd27c780f2114f32
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-11-11 16:21:20 +0300

    Clean up prelude

--- library/Hasql/TH/Prelude.hs
+++ library/Hasql/TH/Prelude.hs
@@ -72,6 +72,7 @@ import Text.ParserCombinators.ReadPrec as Exports (ReadPrec, readPrec_to_P, read
 import Text.Printf as Exports (printf, hPrintf)
 import Text.Read as Exports (Read(..), readMaybe, readEither)
 import Unsafe.Coerce as Exports
+
 -- bytestring
 -------------------------
 import Data.ByteString as Exports (ByteString)
