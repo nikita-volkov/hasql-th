12df0c2b8275ed9b9ebb2d9fa0ba66f501c04295
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-11-13 12:47:39 +0300

    Init with Hasql exps

--- library/Hasql/TH/Exp.hs
+++ library/Hasql/TH/Exp.hs
@@ -3,7 +3,9 @@ module Hasql.TH.Exp where
 import Hasql.TH.Prelude hiding (sequence_, pure, string, list)
 import Language.Haskell.TH
 import qualified Hasql.TH.Prelude as Prelude
+import qualified Hasql.TH.Syntax.Ast as Ast
 import qualified Data.ByteString as ByteString
+import qualified Hasql.Encoders as Encoders
 
 
 byteString :: ByteString -> Exp
@@ -29,3 +31,33 @@ pure = AppE (VarE 'Prelude.pure) (TupE [])
 
 andThen :: Exp -> Exp -> Exp
 andThen exp1 exp2 = AppE (AppE (VarE '(*>)) exp1) exp2
+
+encoderByAstType :: Ast.Type -> Either Text Exp
+encoderByAstType (Ast.Type nullable name arrayLevels) = let
+  paramsEncoder =
+    AppE (VarE 'Encoders.param) .
+    AppE (VarE (if nullable then 'Encoders.nullable else 'Encoders.nonNullable))
+  valueEncoder = case name of
+    "bool" -> Right (VarE 'Encoders.bool)
+    "int2" -> Right (VarE 'Encoders.int2)
+    "int4" -> Right (VarE 'Encoders.int4)
+    "int8" -> Right (VarE 'Encoders.int8)
+    "float4" -> Right (VarE 'Encoders.float4)
+    "float8" -> Right (VarE 'Encoders.float8)
+    "numeric" -> Right (VarE 'Encoders.numeric)
+    "char" -> Right (VarE 'Encoders.char)
+    "text" -> Right (VarE 'Encoders.text)
+    "bytea" -> Right (VarE 'Encoders.bytea)
+    "date" -> Right (VarE 'Encoders.date)
+    "timestamp" -> Right (VarE 'Encoders.timestamp)
+    "timestamptz" -> Right (VarE 'Encoders.timestamptz)
+    "time" -> Right (VarE 'Encoders.time)
+    "timetz" -> Right (VarE 'Encoders.timetz)
+    "interval" -> Right (VarE 'Encoders.interval)
+    "uuid" -> Right (VarE 'Encoders.uuid)
+    "inet" -> Right (VarE 'Encoders.inet)
+    "json" -> Right (VarE 'Encoders.json)
+    "jsonb" -> Right (VarE 'Encoders.jsonb)
+    "enum" -> Right (VarE 'Encoders.enum)
+    _ -> Left ("No value encoder exists for type: " <> name)
+  in fmap paramsEncoder valueEncoder
