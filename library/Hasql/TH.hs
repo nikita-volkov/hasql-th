5ba8125ad41137f3ff00e1cd3fc5f01584821fcb
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-11-14 08:49:32 +0300

    Factor out the exp helper

--- library/Hasql/TH.hs
+++ library/Hasql/TH.hs
@@ -1,18 +1,27 @@
 module Hasql.TH
 where
 
-import Hasql.TH.Prelude
+import Hasql.TH.Prelude hiding (exp)
 import Language.Haskell.TH.Syntax
 import Language.Haskell.TH.Quote
 import qualified Hasql.TH.Exp as Exp
 import qualified Data.Text.Encoding as Text
 
 
+-- * Helpers
+-------------------------
+
+exp :: (String -> Q Exp) -> QuasiQuoter
+exp = let
+  _unsupported _ = fail "Unsupported"
+  in \ _exp -> QuasiQuoter _exp _unsupported _unsupported _unsupported
+
+
+-- * Plain SQL
+-------------------------
+
 sql :: QuasiQuoter
-sql = let
-  exp = return . Exp.byteString . Text.encodeUtf8 . fromString
-  unsupported _ = fail "Unsupported"
-  in QuasiQuoter exp unsupported unsupported unsupported
+sql = exp $ return . Exp.byteString . Text.encodeUtf8 . fromString
 
 {-|
 Read an SQL-file, containing multiple statements,
