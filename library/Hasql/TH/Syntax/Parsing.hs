c3dea9796cb6118c25b0a572100e540e10cbc958
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-02-06 14:18:16 +0300

    Make the trailingJoinedTable parser non-greedy

--- library/Hasql/TH/Syntax/Parsing.hs
+++ library/Hasql/TH/Syntax/Parsing.hs
@@ -859,7 +859,7 @@ trailingJoinedTable _tr1 = asum [
       keyphrase "cross join"
       endHead
       space1
-      _tr2 <- tableRef
+      _tr2 <- nonTrailingTableRef
       return (MethJoinedTable CrossJoinMeth _tr1 _tr2)
     ,
     do
@@ -877,7 +877,7 @@ trailingJoinedTable _tr1 = asum [
       space1
       _jt <- joinTypedJoin
       space1
-      _tr2 <- tableRef
+      _tr2 <- nonTrailingTableRef
       return (MethJoinedTable (NaturalJoinMeth _jt) _tr1 _tr2)
   ]
   where
