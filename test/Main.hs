b708569f9411e9100e8d9c0ab9cb2e15ad0cdf76
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-02-06 11:03:38 +0300

    Reproduce a bug

--- test/Main.hs
+++ test/Main.hs
@@ -13,6 +13,24 @@ import qualified Data.Text as Text
 
 
 main = defaultMain [
+    checkParallel $ Group "Bug-fixes" $ let
+      parsesFine testName sql =
+        (,) (testName <> " parses fine") $ property $ test $ do
+          footnote (Text.unpack sql)
+          case Parsing.run Parsing.preparableStmt sql of
+            Left err -> do
+              footnote err
+              failure
+            Right _ -> success
+      in [
+          parsesFine "Multiple inner joins"
+            "select i :: int8 from auth.user as u\n\
+            \inner join edgenode.usere_provider as p\n\
+            \on u.id = p.user_id\n\
+            \inner join edgenode.provider_branch as b\n\
+            \on b.provider_fk = p.provider_id"
+        ]
+    ,
     checkParallel $ Group "Parsing a rendered AST produces the same AST" $ let
       p _name _amount _gen _parser _renderer =
         (,) _name $ withDiscards (fromIntegral _amount * 200) $ withTests _amount $ property $ do
