a74d4639ea70d659346faef7ecda914524f170c1
Markus Schirp <mbj@schirp-dso.com>
2023-06-18 02:48:39 +0000

    Remove unused imports

--- src/MHasql/TH/Construction/Exp.hs
+++ src/MHasql/TH/Construction/Exp.hs
@@ -4,12 +4,11 @@ module MHasql.TH.Construction.Exp where
 
 import qualified Data.ByteString as ByteString
 import qualified Data.ByteString.Unsafe as ByteString
-import qualified Data.List.NonEmpty as NonEmpty
 import qualified Data.Vector.Generic as Vector
 import qualified Hasql.Decoders as Decoders
 import qualified Hasql.Encoders as Encoders
 import qualified Hasql.Statement as Statement
-import MHasql.TH.Prelude hiding (list, sequence_, string)
+import MHasql.TH.Prelude hiding (sequence_)
 import qualified MHasql.TH.Prelude as Prelude
 import Language.Haskell.TH.Syntax
 import qualified TemplateHaskell.Compat.V0208 as Compat
