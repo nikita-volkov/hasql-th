0b72a47e09f9eb8a093120ee1e342a0773323871
Markus Schirp <mbj@schirp-dso.com>
2023-06-18 00:44:40 +0000

    Add stack setup

---
+++ ci/download-stack.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+curl --location --output stack https://github.com/commercialhaskell/stack/releases/download/v2.11.1/stack-2.11.1-linux-x86_64-bin
+
+echo '5c7a9f86c126d4af15d03ddc03f95f3ceafedd3b9d3dcc7818f2ea60ed6a0ef9  stack' | sha256sum -c
+
+chmod +x ./stack
