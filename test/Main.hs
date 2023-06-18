6e7da303108fec8802868b2ef2a2cea411260a4b
Markus Schirp <mbj@schirp-dso.com>
2023-06-18 02:33:18 +0000

    Add basic smoke test

---
+++ test/Main.hs
@@ -0,0 +1,30 @@
+import MPrelude
+import MIO.Core (runMIO)
+
+import qualified CBT
+import qualified CBT.Container
+import qualified DBT.Postgresql            as DBT
+import qualified DBT.Postgresql.Connection as DBT
+import qualified DBT.Postgresql.Container  as DBT
+import qualified Data.ByteString.Lazy      as LBS
+import qualified Data.Text.Encoding        as Text
+import qualified Hasql.Statement           as Hasql
+import qualified Hasql.Session             as Hasql
+import qualified MHasql.TH                 as MHasql
+import qualified Test.Tasty                as Tasty
+import qualified Test.Tasty.HUnit          as Tasty
+
+main :: IO ()
+main = do
+  CBT.runDefaultEnvironment $ do
+    containerName <- CBT.Container.nextName (CBT.Container.Prefix "mhasql-test")
+    DBT.withDatabaseContainerDefault containerName $ \clientConfig ->
+      liftIO . Tasty.defaultMain $ Tasty.testGroup "mhasql"
+        [ testDB clientConfig
+        ]
+
+testDB :: DBT.ClientConfig -> Tasty.TestTree
+testDB clientConfig =
+  Tasty.testCase "smoke test" $
+    Tasty.assertEqual "" True =<< runMIO ()
+      (DBT.withConnectionSession clientConfig $ Hasql.statement () [MHasql.singletonStatement|SELECT true :: bool|])
