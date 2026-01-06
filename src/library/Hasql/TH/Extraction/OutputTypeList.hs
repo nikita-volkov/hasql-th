6e86142f9bd47e9fdae27ff7f65090f01e656b61
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-01-07 01:15:29 +0300

    Suppress various compiler warnings for internal modules

--- src/library/Hasql/TH/Extraction/OutputTypeList.hs
+++ src/library/Hasql/TH/Extraction/OutputTypeList.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Wno-missing-signatures #-}
+{-# OPTIONS_GHC -Wno-unused-binds -Wno-unused-imports -Wno-name-shadowing -Wno-incomplete-patterns -Wno-unused-matches -Wno-missing-methods -Wno-unused-record-wildcards -Wno-redundant-constraints -Wno-deprecations -Wno-missing-signatures #-}
 
 -- |
 -- AST traversal extracting output types.
