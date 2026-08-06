b6761487a9e877e0b6eaf27b27224b6dc0777ad2
nikita-volkov <1560937+nikita-volkov@users.noreply.github.com>
2026-08-06 09:36:31 +0000

    Format

--- src/library/Hasql/TH/Prelude.hs
+++ src/library/Hasql/TH/Prelude.hs
@@ -70,6 +70,7 @@ import GHC.Generics as Exports (Generic, Generic1)
 import GHC.IO.Exception as Exports
 import Hasql.Statement as Exports (Statement)
 import Numeric as Exports
+import Prelude as Exports hiding (all, and, any, concat, concatMap, elem, fail, foldl, foldl1, foldr, foldr1, id, mapM, mapM_, maximum, minimum, notElem, or, product, sequence, sequence_, sum, (.))
 import System.Environment as Exports
 import System.Exit as Exports
 import System.IO as Exports
@@ -81,7 +82,6 @@ import System.Timeout as Exports
 import Text.Printf as Exports (hPrintf, printf)
 import Text.Read as Exports (Read (..), readEither, readMaybe)
 import Unsafe.Coerce as Exports
-import Prelude as Exports hiding (all, and, any, concat, concatMap, elem, fail, foldl, foldl1, foldr, foldr1, id, mapM, mapM_, maximum, minimum, notElem, or, product, sequence, sequence_, sum, (.))
 
 showAsText :: (Show a) => a -> Text
 showAsText = show >>> fromString
